3aabe60002de3c9b7cb2744f087a0fda76d3c01f
Mike Chang <mchang95@gmail.com>
2021-07-08 12:24:56 -0700

    adds questions redirect and completion page

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ responses = []
 
 @app.route("/")
 def start():
+    """ Create homepage with start button. """
 
     return render_template("survey_start.html",
                            survey_title=survey.title,
@@ -21,21 +22,39 @@ def start():
 
 @app.route("/begin", methods=["POST"])
 def survey_begin():
+    """ Redirects user to the first question of correct survey. """
 
     return redirect("/questions/0")
 
 
 @app.route("/questions/<int:question_id>")
 def questions(question_id):
+    """ Generates and returns question page from question_id. """
 
     return render_template("question.html",
-                           question=survey.questions[question_id])
+                           question=survey.questions[question_id],
+                           question_id=question_id)
 
 
 @app.route("/answer", methods=["POST"])
 def answer_page():
+    """ Appends answer and redirects to next question
+        if next question outside of range of questions, redirect
+        to completion page.
+    """
 
-    ans = request.form["answer"]
-    responses.append(ans)
+    responses.append(request.form["answer"])
 
-    return ans
+    new_id = int(request.form["question-id"]) + 1
+
+    if new_id < len(survey.questions):
+        return redirect(f"/questions/{new_id}")
+    else:
+        return redirect("/completion")
+
+
+@app.route("/completion")
+def completed_survey():
+    """ Provides confirmation of survey completion. """
+
+    return render_template("completion.html")
