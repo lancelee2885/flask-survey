6e13acd85c850deb9f1a573606f970d6b67017ec
lancelee2885 <lancelee2885@gmail.com>
2021-07-08 12:02:29 -0700

    step4 finished

--- app.py
+++ app.py
@@ -7,3 +7,35 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route("/")
+def start():
+
+    return render_template("survey_start.html",
+                           survey_title=survey.title,
+                           survey_instructions=survey.instructions)
+
+
+@app.route("/begin", methods=["POST"])
+def survey_begin():
+
+    return redirect("/questions/0")
+
+
+@app.route("/questions/<int:question_id>")
+def questions(question_id):
+
+    return render_template("question.html",
+                           question=survey.questions[question_id])
+
+
+@app.route("/answer", methods=["POST"])
+def answer_page():
+
+    ans = request.form["answer"]
+    responses.append(ans)
+
+    return ans
